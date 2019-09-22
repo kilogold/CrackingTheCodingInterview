739a6b0960352c8f2388251581f27dfb5ee445d4
Kelvin Bonilla <bonilla_kelvin@hotmail.com>
2019-09-22 14:09:38 -0400

    Added more test cases. Palindome.

--- UnitTest/Exercises/ArraysAndStrings.h
+++ UnitTest/Exercises/ArraysAndStrings.h
@@ -47,4 +47,70 @@ namespace CTCI
 
 		return true;
 	}
+
+	void Urlify(char* s, int len)
+	{
+		if (!s) return;
+
+		auto swap = [](int a, int b, char* s)
+		{
+			s[a] ^= s[b];
+			s[b] ^= s[a];
+			s[a] ^= s[b];
+		};
+		auto loopSwap = [&swap](int start, int end, char* s)
+		{
+			for (int i = end; i > start+1; i -= 2)
+			{
+				swap(i - 2, i, s);
+				swap(i - 1, i + 1, s);
+			}
+		};
+		
+		int end = len;
+		size_t totalStrLen = strlen(s);
+		for (int i = 0; end < totalStrLen; ++i)
+		{
+			if (s[i] == ' ')
+			{
+				loopSwap(i, end, s);
+				s[i] = '%';
+				s[i + 1] = '2';
+				s[i + 2] = '0';
+
+				i += 2;
+				end += 2;
+			}
+		}
+	}
+
+	bool IsPalindrome(const char* s)
+	{
+		if (!s) return false;
+
+		const size_t len = strlen(s);
+		std::unordered_set<char> look;
+		look.reserve(len);
+
+		for (size_t i = 0; i < len; ++i)
+		{
+			if (look.find(s[i]) != look.end())
+			{
+				look.erase(s[i]);
+			}
+			else
+			{
+				look.insert(s[i]);
+			}
+		}
+
+		const bool isOddLen = ((len & 1) == 1);
+
+		if (isOddLen && look.size() == 1) 
+			return true;
+		if (!isOddLen && look.size() == 0)
+			return true;
+
+		return false;
+	}
 }
--- UnitTest/main.cpp
+++ UnitTest/main.cpp
@@ -22,5 +22,31 @@ namespace Excercises
 			Assert::IsFalse(CTCI::CheckPermutation("trap", "help"), L"Not a permutation.", LINE_INFO());
 			Assert::IsTrue(CTCI::CheckPermutation("listen", "silent"), L"Is a permutation.", LINE_INFO());
 		}
+
+		TEST_METHOD(Urlify)
+		{
+			{
+				const char* t0 = "Hello%20World";
+				char* t1 = new char[14];
+				strcpy(t1, "Hello World  ");
+				CTCI::Urlify(t1, 11);
+				Assert::AreEqual(t0, t1, "Not equal");
+				delete[] t1;
+			}
+			{
+				const char* t0 = "This%20is%20a%20more%20complex%20%20%20sentence.";
+				char* t1 = new char[49];
+				strcpy(t1, "This is a more complex   sentence.              ");
+				CTCI::Urlify(t1, 34);
+				Assert::AreEqual(t0, t1, "Not equal");
+				delete[] t1;
+			}
+		}
+
+		TEST_METHOD(IsPalindrome)
+		{
+			Assert::IsTrue(CTCI::IsPalindrome("RACECAR"), L"Is a palindrome");
+			Assert::IsFalse(CTCI::IsPalindrome("MIKELSEN"), L"Is not a palindrome");
+		}
 	};
 }
